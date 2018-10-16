f83b59675f44c354094dc0a1b0a87633c1d2ba11
guerrk1n <aleksej684549@inbox.ru>
2018-10-16 16:39:56 +0300

    тест1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,8 +8,6 @@ public class Main {
 
 
         LinkList polynomial = new LinkList();
-        LinkList polynomial2 = new LinkList();
-        LinkList polynomial3 = new LinkList();
 
         String polynomial_string = "2x^2-3x+4";
 
@@ -19,10 +17,10 @@ public class Main {
 
                 if (i >= 2 && polynomial_string.charAt(i - 2) == '-') {
 
-                    polynomial.insertFirst(-Character.getNumericValue(polynomial_string.charAt(i - 1)), polynomial_string.charAt(i + 2));
+                    polynomial.insertFirst(-Character.getNumericValue(polynomial_string.charAt(i - 1)), Character.getNumericValue(polynomial_string.charAt(i + 2)));
 
                 } else {
-                    polynomial.insertFirst(polynomial_string.charAt(i - 1), polynomial_string.charAt(i + 2));
+                    polynomial.insertFirst(Character.getNumericValue(polynomial_string.charAt(i - 1)), Character.getNumericValue(polynomial_string.charAt(i + 2)));
                 }
 
             } else if (polynomial_string.charAt(i) == 'x' && polynomial_string.charAt(i + 1) != '^') { // X в первой степени
@@ -32,22 +30,20 @@ public class Main {
                     polynomial.insertFirst(-Character.getNumericValue(polynomial_string.charAt(i - 1)), 1);
 
                 } else {
-                    polynomial.insertFirst(polynomial_string.charAt(i - 1), 1);
+                    polynomial.insertFirst(Character.getNumericValue(polynomial_string.charAt(i - 1)), 1);
                 }
-            }
-
-            if (polynomial.isDigit(polynomial_string.charAt(i)) && polynomial_string.charAt(i + 1) != 'x' && i<polynomial_string.length()) { // свободный член
+            } else if (polynomial.isDigit(polynomial_string.charAt(i))) { // свободный член
 
                 if (i >= 2 && polynomial_string.charAt(i - 2) == '-') {
 
                     polynomial.insertFirst(-Character.getNumericValue(polynomial_string.charAt(i)), 0);
                 } else {
-                    polynomial.insertFirst(polynomial_string.charAt(i), 0);
+                    polynomial.insertFirst(Character.getNumericValue(polynomial_string.charAt(i)), 0);
                 }
             }
 
 
-
+            polynomial.displayList();
 
 
         }
